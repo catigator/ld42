5373afab313019310164d1bcd37f8af244b5d3aa
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2018-08-11 15:11:15 +0100

    Can shoot torpedos!

--- Assets/PlayerShooter.cs
+++ Assets/PlayerShooter.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour {
 
+	public float elapsedTime;
+	public float bulletInterval;
+	public GameObject bullet;
+	public float velocity = 20f;
+	public AudioManager am;
+
 	// Use this for initialization
 	void Start () {
-		
+		am = GameObject.Find("AudioManager").GetComponent<AudioManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		elapsedTime += Time.deltaTime;
+	    if (elapsedTime > bulletInterval)
+	    {
+	        if (Input.GetKey(KeyCode.Space))
+	        {
+				// am.playerShoot.Play();
+	            GameObject newBullet = Instantiate(
+					bullet, transform.position, transform.rotation) as GameObject;
+
+				Vector3 newPosition = newBullet.transform.position;
+				newPosition.y -= 0.4f;
+				newBullet.transform.position = newPosition;
+
+	            Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
+	            rb.velocity = transform.right*velocity;
+
+                elapsedTime = 0f;
+	        }
+	    }
 	}
 }
