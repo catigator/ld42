38e9ec7053aced4ee5a140e6d9a3115190af3e47
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2018-08-11 15:57:03 +0100

    Enemies swim towards player!

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public GameObject player;
+	public Rigidbody2D rb;
+
+	public Vector2 vectorToPlayer;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		rb = this.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vectorToPlayer = player.transform.position - this.transform.position;
+
+		rb.velocity = vectorToPlayer.normalized*speed;
+	}
+}
